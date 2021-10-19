1a654c949df7adda617aca712abd43e003b4f22f
devilmah <91089885+devilmah@users.noreply.github.com>
2021-10-19 19:52:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function GetOptions()
+{
+	
+	document.getElementById('options').innerHTML="boo"; /*<p><input name="option" type="radio" value="belg"> Бельгийские</p>
+												  <p><input name="option" type="radio" value="ven"> Венские</p>
+												  <p><input name="option" type="radio" value="gol"> Голландские</p>;*/
+}
